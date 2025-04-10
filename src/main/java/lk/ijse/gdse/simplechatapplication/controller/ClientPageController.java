9defb1a7a06902b5e6b52955aa96ff87f717f53e
hasinduudara <hasindutechschool@gmail.com>
2025-04-10 12:37:35 +0530

    update

--- src/main/java/lk/ijse/gdse/simplechatapplication/controller/ClientPageController.java
+++ src/main/java/lk/ijse/gdse/simplechatapplication/controller/ClientPageController.java
@@ -5,12 +5,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 public class ClientPageController {
 
@@ -26,6 +29,12 @@ public class ClientPageController {
     @FXML
     private TextField clientTextField;
 
+    @FXML
+    private ImageView ImageViwe;
+
+    @FXML
+    private Button btnSendFile;
+
     private Socket socket;
     private DataInputStream dataInputStream;
     private DataOutputStream dataOutputStream;
@@ -37,14 +46,17 @@ public class ClientPageController {
                 dataInputStream = new DataInputStream(socket.getInputStream());
                 dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
-                String message;
-                while ((message = dataInputStream.readUTF()) != null) {
-                    String finalMessage = message;
-                    javafx.application.Platform.runLater(() ->
-                            clientTextArea.appendText("Server: " + finalMessage + "\n")
-                    );
+                while (true) {
+                    String message = dataInputStream.readUTF();
+                    if (message.equals("Image")) {
+                        int fileByteLength = dataInputStream.readInt();
+                        byte[] fileBytes = new byte[fileByteLength];
+                        dataInputStream.readFully(fileBytes);
+                        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(fileBytes);
+                        ImageViwe.setImage(new Image(byteArrayInputStream));
+                    }
+                    clientTextArea.appendText("server: " + message + "\n");
                 }
-
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -60,6 +72,26 @@ public class ClientPageController {
             clientTextField.clear();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void btnSendFileOnAction(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select File to Send");
+        File file = fileChooser.showOpenDialog(new Stage());
+        System.out.println(file.getName());
+        if (file != null) {
+            try {
+                byte[] fileBytes = Files.readAllBytes(file.toPath());
+                dataOutputStream.writeUTF("Image");
+                dataOutputStream .writeInt(fileBytes.length);
+                dataOutputStream.write(fileBytes);
+                dataOutputStream.flush();
+                clientTextArea.appendText("Client: Sent file " + file.getName() + "\n");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
--- src/main/java/lk/ijse/gdse/simplechatapplication/controller/ServerPageController.java
+++ src/main/java/lk/ijse/gdse/simplechatapplication/controller/ServerPageController.java
@@ -5,11 +5,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -27,12 +29,18 @@ public class ServerPageController {
     @FXML
     private TextField serverTextField;
 
+    @FXML
+    private ImageView ImageViwe;
+
+    @FXML
+    private Button btnSendFile;
+
     private ServerSocket serverSocket;
     private Socket socket;
     private DataInputStream dataInputStream;
     private DataOutputStream dataOutputStream;
 
-    public void initialize() {
+    public void initialize() throws IOException {
         new Thread(() -> {
             try {
                 serverSocket = new ServerSocket(1234);
@@ -40,19 +48,24 @@ public class ServerPageController {
                 dataInputStream = new DataInputStream(socket.getInputStream());
                 dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
-                String message;
-                while ((message = dataInputStream.readUTF()) != null) {
-                    String finalMessage = message;
-                    javafx.application.Platform.runLater(() ->
-                            serverTextArea.appendText("Client: " + finalMessage + "\n")
-                    );
-                }
+                while (true) {
+                    String message = dataInputStream.readUTF();
 
+                    if (message.equals("Image")) {
+                        int fileByteLength = dataInputStream.readInt();
+                        byte[] fileBytes = new byte[fileByteLength];
+                        dataInputStream.readFully(fileBytes);
+                        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(fileBytes);
+                        ImageViwe.setImage(new Image(byteArrayInputStream));
+                    }
+                    serverTextArea.appendText("client: " + message + "\n");
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }).start();
     }
+
 
     @FXML
     void btnserverSendOnAction(ActionEvent event) {
@@ -63,6 +76,26 @@ public class ServerPageController {
             serverTextField.clear();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void btnSendFileOnAction(ActionEvent event) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select File to Send");
+        File file = fileChooser.showOpenDialog(new Stage());
+        System.out.println(file.getName());
+        if (file != null) {
+            try {
+                byte[] fileBytes = new byte[(int) file.length()];
+                dataOutputStream.writeUTF("Image");
+                dataOutputStream .writeInt(fileBytes.length);
+                dataOutputStream.write(fileBytes);
+                dataOutputStream.flush();
+                serverTextArea.appendText("Server: Sent file " + file.getName() + "\n");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
